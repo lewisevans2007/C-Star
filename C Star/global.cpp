5c6ece7fc7b6758fc2f732085e4e7bf678510f9f
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-12-26 11:25:58 +0000

    Add main header

--- C Star/global.cpp
+++ C Star/global.cpp
@@ -5,5 +5,5 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
-std::string ver ="0.1.3.0";
+std::string ver ="0.1.4.0";
 std::string build_type = "BETA";
---
+++ C Star/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+string function(string line, vector<string> imported, vector<string> variables, vector<string> variables_values)
+#endif
